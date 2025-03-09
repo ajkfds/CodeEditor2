5da2ef7dcaf89a1b44b23d547fdf0214b26dbdca
ajkfds <ajkfds2018@gmail.com>
2025-03-10 05:53:01 +0900

    add markdown preview

---
+++ CodeEditor2/CodeEditor2/Tools/BrowserWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CodeEditor2;
+
+public partial class BrowserWindow : Window
+{
+    public BrowserWindow()
+    {
+        InitializeComponent();
+    }
+}
