07c561d6bf83803e0623b079281bf1d536c33e40
ajkfds <ajkfds2018@gmail.com>
2025-08-16 13:09:22 +0900

    support multiline log message

--- CodeEditor2/CodeEditor2/Views/LogView.axaml.cs
+++ CodeEditor2/CodeEditor2/Views/LogView.axaml.cs
@@ -1,6 +1,7 @@
 using AjkAvaloniaLibs.Controls;
 using Avalonia.Controls;
 using Avalonia.Threading;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CodeEditor2.Views
@@ -18,15 +19,23 @@ namespace CodeEditor2.Views
 
         public void AppendLog(string message)
         {
-            AjkAvaloniaLibs.Controls.ListViewItem item = new AjkAvaloniaLibs.Controls.ListViewItem(message);
-            Dispatcher.UIThread.Post(() => appendLog(item));
-            
+            List<string> messages = message.Replace("\r", "").Split('\n', System.StringSplitOptions.RemoveEmptyEntries).ToList();
+            foreach (string m in messages)
+            {
+                AjkAvaloniaLibs.Controls.ListViewItem item = new AjkAvaloniaLibs.Controls.ListViewItem(m);
+                Dispatcher.UIThread.Post(() => appendLog(item));
+            }
         }
 
         public void AppendLog(string message,Avalonia.Media.Color color)
         {
-            AjkAvaloniaLibs.Controls.ListViewItem item = new AjkAvaloniaLibs.Controls.ListViewItem(message, color);
-            Dispatcher.UIThread.Post(() => appendLog(item));
+            List<string> messages = message.Replace("\r", "").Split('\n',System.StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            foreach(string m in messages)
+            {
+                AjkAvaloniaLibs.Controls.ListViewItem item = new AjkAvaloniaLibs.Controls.ListViewItem(m, color);
+                Dispatcher.UIThread.Post(() => appendLog(item));
+            }
         }
 
         private void appendLog(ListViewItem item)
