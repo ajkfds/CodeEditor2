dce13c55c75977ace6dbc136b30483c5011a5514
ajkfds <ajkfds2018@gmail.com>
2025-12-31 07:43:10 +0900

    add llm interface

---
+++ CodeEditor2/CodeEditor2/LLM/ILlmOnBrowser.cs
@@ -0,0 +1,27 @@
+﻿using Svg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEditor2.LLM
+{
+    public interface ILlmOnBrowser
+    {
+        public void Launch()
+        {
+
+        }
+        public void Reset()
+        {
+
+        }
+
+        public Task<string?> Ask(string prompt)
+        {
+            return Task.FromResult<string?>(null);
+        }
+
+    }
+}
