d166a42f32d4cbeb22e0ed20eb97d88286068fac
ajkfds <ajkfds2018@gmail.com>
2025-12-31 07:43:25 +0900

    add filetype select

---
+++ CodeEditor2/CodeEditor2/Views/FileTypeView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CodeEditor2;
+
+public partial class FileTypeView : UserControl
+{
+    public FileTypeView()
+    {
+        InitializeComponent();
+    }
+}
