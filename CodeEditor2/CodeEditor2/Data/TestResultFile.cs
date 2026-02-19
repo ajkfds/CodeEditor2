f24053d7afb84879e7d642c7a5f0b434aeba7273
ajkfds <ajkfds2018@gmail.com>
2026-02-20 06:19:15 +0900

    add test result file

---
+++ CodeEditor2/CodeEditor2/Data/TestResultFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEditor2.Data
+{
+    public class TestResultFile : TextFile
+    {
+        public static async Task<TestResultFile> CreateAsync(string relativePath, Project project)
+        {
+            string name;
+            if (relativePath.Contains(System.IO.Path.DirectorySeparatorChar))
+            {
+                name = relativePath.Substring(relativePath.LastIndexOf(System.IO.Path.DirectorySeparatorChar) + 1);
+            }
+            else
+            {
+                name = relativePath;
+            }
+            TestResultFile fileItem = new TestResultFile()
+            {
+                Project = project,
+                RelativePath = relativePath,
+                Name = name
+            };
+            await fileItem.FileCheck();
+            if (fileItem.document == null) System.Diagnostics.Debugger.Break();
+            return fileItem;
+        }
+    }
+}
---
+++ CodeEditor2/CodeEditor2/FileTypes/TestResultFile.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEditor2.FileTypes
+{
+    public class TestResultFile : FileType
+    {
+        public override string ID { get { return "TestResultFile"; } }
+
+        public override bool IsThisFileType(string relativeFilePath, Data.Project project)
+        {
+            if (
+                relativeFilePath.ToLower().EndsWith(".testresult")
+            )
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override async Task<Data.File?> CreateFile(string relativeFilePath, Data.Project project)
+        {
+            return await Data.TestResultFile.CreateAsync(relativeFilePath, project);
+        }
+
+        public override IImage GetIconImage()
+        {
+            return AjkAvaloniaLibs.Libs.Icons.GetSvgBitmap(
+                "CodeEditor2/Assets/Icons/text.svg",
+                Avalonia.Media.Color.FromArgb(100, 200, 200, 200)
+                );
+        }
+
+    }
+}
